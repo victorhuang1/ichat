ac46307eaf390fcf4125b59bbd45e952fa02e49c
victorhuang1 <geekle@yeah.net>
2021-07-04 03:38:45 +0800

    first commit

---
+++ package/index.js
@@ -0,0 +1,6 @@
+export const default_avatar = require('./images/default_avatar.png');
+
+export const default_unnoticed = require("./images/unnotic.png");
+
+export const tipSound = require('./media/tip.wav');
+
---
+++ packages/index.js
@@ -0,0 +1,21 @@
+import "./styles/index.scss";
+
+const components = [];
+
+const install = function (Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  components.forEach((component) => {
+    Vue.component(component.name, component);
+  });
+};
+
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  version: "1.0.0",
+  install,
+  ...components,
+};
